410e0243c495db88f2d4311c5240714008de0177
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-01 15:24:59 +0530

    Add files via upload
    Document Header Added

---
+++ anonymous.py
@@ -0,0 +1,18 @@
+'''
+Author : Pranav S Nair
+Date : 01-10-2024
+Python program to get the student details
+Version : 1.0
+'''
+
+
+
+
+Name = input("Enter the name of the student : ")
+Roll_no = int(input("Enter the roll number : "))
+cgpa = float(input("Enter CGPA : "))
+percentage = cgpa * 10
+Roll_no = Roll_no + 1
+print("Name of the student is ",Name)
+print("Roll number : ",Roll_no )
+print("Percentage is ",percentage,"%")
