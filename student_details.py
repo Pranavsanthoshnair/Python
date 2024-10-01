bc6dff382fe54d81fb948d89ef25af6ce834fada
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-01 15:39:19 +0530

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -12,7 +12,6 @@ Name = input("Enter the name of the student : ")
 Roll_no = int(input("Enter the roll number : "))
 cgpa = float(input("Enter CGPA : "))
 percentage = cgpa * 10
-Roll_no = Roll_no + 1
 print("Name of the student is ",Name)
 print("Roll number : ",Roll_no )
 print("Percentage is ",percentage,"%")
