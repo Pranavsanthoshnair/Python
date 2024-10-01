5cca1db980efbfb219782a0a778f6f9bcabb284f
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-01 19:12:46 +0530

    Add files via upload

---
+++ Twinkle twinkle.py
@@ -0,0 +1,12 @@
+"""
+ Python program to print a string in a specific format
+Author - Pranav S Nair
+Date - 01-10-2024
+"""
+
+
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+
+
+
+
