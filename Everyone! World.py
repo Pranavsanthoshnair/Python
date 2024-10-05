d2b6aa8d5fb87a3652d4192cbcaef75fb7d97b03
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-05 15:32:55 +0530

    Add files via upload

---
+++ Everyone! World.py
@@ -0,0 +1,11 @@
+"""
+A python program that stores a string in the variable, Extracts a part of the string(substring) and then concatenates it with another string
+Author - Pranav S Nair
+Date - 05/10/2024
+"""
+
+str1 = "Hello, World!"
+str2 = str1[7:12]
+str3 = " Everyone!"
+str4 = str3 + " " + str2
+print(str4)
