8386b7c00bbfee1253af8a5fd6b265751349f22e
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-05 15:34:05 +0530

    Add files via upload

---
+++ student_name and age.py
@@ -0,0 +1,11 @@
+"""
+A python program which asks user to input name and age and then prints the same
+Author - Pranav S Nair
+Date - 05/10/2024
+"""
+
+
+student_name = input("Enter the student's  : ")
+student_age = input("Enter the student's age : ")
+
+print("Hello,",student_name,"\b!","You are ",student_age,"years old.")  #Here if you print without using the escape sequence "\b",The exclamation mark will come with a space after your name.
---
+++ usage of operators.py
@@ -0,0 +1,18 @@
+"""
+A python program which demonstrate the use of operators
+Author - Pranav S Nair
+Date - 05/10/2024
+"""
+
+a = 10
+b = 5
+sum = a + b
+division = a / b
+greater_than = a>b
+equal_to = a==b
+
+
+
+
+print("Sum:",sum,"Division:",division,"\n\nIs a greater than b?:",greater_than,"\n\nAre a and b equal?:",equal_to,"\n\nLogical AND:",b>a and a!=b,"\n\nLogical OR:",a!=b or a>b)
+
