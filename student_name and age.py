c06e0fa8ba37e79f915b3ffd5989d9ee10a3c117
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-06 10:38:04 +0530

    Update student_name and age.py

--- student_name and age.py
+++ student_name and age.py
@@ -5,7 +5,7 @@ Date - 05/10/2024
 """
 
 
-student_name = input("Enter the student's  : ")
+student_name = input("Enter the student's name : ")
 student_age = input("Enter the student's age : ")
 
 print("Hello,",student_name,"\b!","You are ",student_age,"years old.")  #Here if you print without using the escape sequence "\b",The exclamation mark will come with a space after your name.
