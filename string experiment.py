e94727789cbbd8f810c3ce939175b9ffc1540d74
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-08 14:31:06 +0530

    Add files via upload

---
+++ string experiment.py
@@ -0,0 +1,12 @@
+"""
+ A Program to Create, concatenate, and print a string and access a sub-string from a given string.
+Author - Pranav S Nair
+Date - 08/10/2024
+"""
+
+
+first_name = "Pranav"
+last_name = "S Nair"
+new_str = first_name + " " + last_name
+print(new_str)
+print(new_str[7:13])
