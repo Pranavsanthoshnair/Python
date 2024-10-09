f01ddf9f01ff65521119a0e8d23c6e9f6f89711b
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-09 19:43:48 +0530

    Add files via upload

---
+++ grade_calculator.py
@@ -0,0 +1,19 @@
+score = float(input("Enter your score : "))
+
+if(score >= 90):
+    print("A")
+elif(score >= 80):
+    print("B")
+elif(score >= 70):
+    print("C")
+elif(score >= 60):
+    print("D")
+elif(score >= 50):
+    print("E")
+elif(score >= 0):
+    print("Fail")
+else:
+    print("Invalid Score")
+
+
+
