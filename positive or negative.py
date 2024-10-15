9b6fd7b3e52266c9fb8506ae00dd7873b560d49e
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-15 15:00:03 +0530

    Add files via upload

---
+++ positive or negative.py
@@ -0,0 +1,14 @@
+"""
+Pranav S Nair
+15/10/2024
+A Python program that determines whether the number is positive or negative
+"""
+
+num = int(input("Enter any number : "))
+
+if num<0:
+    print("The numbewr is negative")
+elif num>0:
+    print("The number is positive")
+else:
+    print("The number is zero")
