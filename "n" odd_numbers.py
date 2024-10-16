b3ccfcb8b22255f9c35e5ff841adad573edd2dda
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-16 20:35:32 +0530

    Update "n" odd_numbers.py

--- "n" odd_numbers.py
+++ "n" odd_numbers.py
@@ -1,3 +1,9 @@
+"""
+A Python program that prints odd numbers upto n(specified by user
+Author - Pranav S Nair
+Date - 15/10/2024
+"""
+
 limit = int(input("Enter the limit : "))
 odd_number = 1
 count = 0
