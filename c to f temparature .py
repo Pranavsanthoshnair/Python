5d19cac12ea1de30e210e89b02b484fc55b5f2bd
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-22 15:12:35 +0530

    Update c to f temparature .py

--- c to f temparature .py
+++ c to f temparature .py
@@ -1,9 +1,9 @@
 temp = float(input("Enter Temparature : "))
-c_or_f = input("Enter if the temparature is in celsius(C) or fahrenheit(F) : ")
+scale = input("Enter if the temparature is in celsius(C) or fahrenheit(F) : ")
 f_to_c = (5/9)*(temp-32)
 c_to_f = (9/5)*temp+32
 
-if(c_or_f == "c" or c_or_f == "C"):
+if(scale == "c" or scale == "C"):
     print(temp ,"celsius is ",c_to_f ,"fahrenheit")
-elif(c_or_f == "f" or c_or_f == "F"):
+elif(scale == "f" or scale == "F"):
     print(temp ,"fahrenheit is ",f_to_c ,"celsius")
