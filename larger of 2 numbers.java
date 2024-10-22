3cad3ce6f5bd9e4e3c39c4f0f60aa34a8562f34b
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-10-22 15:57:43 +0530

    Add files via upload

---
+++ larger of 2 numbers.java
@@ -0,0 +1,37 @@
+/**
+  * NAME:
+  * DATE:
+  * FILE:
+  * COMMENTS:
+  */
+
+public class larger of 2 numbers extends eecs.Gui
+{
+   public static void main(String[] args)
+   {
+      // declare variables
+      String raptor_prompt_variable_zzyz = null;
+      ?? num1 = ??;
+      ?? num2 = ??;
+      
+      raptor_prompt_variable_zzyz = "Enter first number";
+      num1 = get???(raptor_prompt_variable_zzyz);
+      raptor_prompt_variable_zzyz = "Enter second number";
+      num2 = get???(raptor_prompt_variable_zzyz);
+      if (num1 > num2)
+      {
+         printLine("Biggest number is " + (num1));
+      }
+      else
+      {
+         if (num2 > num1)
+         {
+            printLine("Biggest numer is" + (num2));
+         }
+         else
+         {
+            printLine("both are equal");
+         }
+      }
+   } // close main
+} // close larger of 2 numbers
