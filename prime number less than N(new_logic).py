c727213dba013cb95eff641d5cb2b6e1bbc01955
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-11-23 09:02:46 +0530

    Update prime number less than N(new_logic).py

--- prime number less than N(new_logic).py
+++ prime number less than N(new_logic).py
@@ -4,11 +4,11 @@ Date - 12/11/2024
 A python program to print the prime numbers less than a number.
 """
 
-N = int(input("Enter a number: "))
-print(f"Prime number less than {N} are :")
-for num in range(2,N):
+number = int(input("Enter a number: "))
+print(f"Prime number less than {number} are :")
+for num in range(2,number):
     for i in range(2,num):
-        if num%i == 0:
+        if num % i == 0:
             break
     else:
         print(num, end = " ")   # if for loop comletely finishes it comes to the "else" clause.(logic)
