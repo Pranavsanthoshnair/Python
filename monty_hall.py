b5480835d04eca9620a3d5d51b3ad1b8b4b858c4
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-12-03 18:53:49 +0530

    Update monty_hall.py

--- monty_hall.py
+++ monty_hall.py
@@ -1,7 +1,7 @@
 """
 Author - Pranav S Nair
 Date - 3/12/2024
-A python program to develop a game called monty_hall
+A python program of the game ;monty hall'
 """
 import random
 
@@ -11,19 +11,18 @@ def game_monty_hall():
     print("You will pick a door, and the host will reveal a goat behind one of the other doors.")
     print("Then, you can choose to switch doors or stay with your choice.")
 
-
     doors = ['1', '2', '3']
     prizes = ['car', 'goat', 'goat']
     random.shuffle(prizes)  # Randomize the prize locations
 
-
+    # Player's choice
     player_choice = input("Choose a door (1, 2, or 3): ")
     if player_choice not in doors:
         print("Invalid choice! Please restart and choose a valid door.")
         return
 
-
     player_index = doors.index(player_choice)  # Find the index of the chosen door
+
     # Host reveals a goat from the remaining doors
     for i in range(3):
         if i != player_index and prizes[i] == 'goat':  # Find a door with a goat
@@ -31,16 +30,28 @@ def game_monty_hall():
             break
     print(f"The host opens door {host_reveal} to reveal a goat.")
 
+    # Determine the remaining door that the player could switch to
+    if player_choice == '1' and host_reveal != '2':
+        remaining_door = '2'
+    elif player_choice == '1' and host_reveal != '3':
+        remaining_door = '3'
+    elif player_choice == '2' and host_reveal != '1':
+        remaining_door = '1'
+    elif player_choice == '2' and host_reveal != '3':
+        remaining_door = '3'
+    elif player_choice == '3' and host_reveal != '1':
+        remaining_door = '1'
+    elif player_choice == '3' and host_reveal != '2':
+        remaining_door = '2'
 
-    remaining_door = [d for d in doors if d != player_choice and d != host_reveal][0]
+    # Ask if the player wants to switch doors
     switch = input(f"Do you want to switch to door {remaining_door}? (yes or no): ").lower()
-
 
     if switch == 'yes':
         player_choice = remaining_door
 
-
     final_prize = prizes[doors.index(player_choice)]
+
     if final_prize == 'car':
         print(f"Congratulations! You chose door {player_choice} and won a car!")
     else:
